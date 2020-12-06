0f513de01f21366bb56ef1fb61f12ca603c6a0b2
Glitch (beneficial-steadfast-comfort) <none>
2020-12-06 14:48:34 +0000

    🍗🚠 Checkpoint ./myApp.js:25933543/294

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get(
-  '/:word/echo', (req, res) => {
+  '/name?first=karthik&last=vellur', (req, res) => {
     res.send({echo: req.params.word})
   }
 )
