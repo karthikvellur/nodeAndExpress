c2f4535f5c9eedce21b28dc19fe02f8eea9857b1
Glitch (beneficial-steadfast-comfort) <none>
2020-12-06 15:00:20 +0000

    🏝👹 Checkpoint ./myApp.js:25933543/537

--- myApp.js
+++ myApp.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
 
 app.get(
-  '/name?first=karthik&last=vellur', (req, res) => {
+  '/:word/echo', (req, res) => {
     res.send({echo: req.params.word})
   }
 )
+
+app.get(
+  '/name', (req, res) => {
+    res.send({name: `${req.query.first} ${req.query.last}`})
+  }
+)
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 
