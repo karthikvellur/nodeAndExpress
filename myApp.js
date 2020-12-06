cac2879aff773657f0abecbf3698ae2a0222e232
Glitch (beneficial-steadfast-comfort) <none>
2020-12-06 14:45:59 +0000

    route parameter

--- myApp.js
+++ myApp.js
@@ -1,15 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get("/now",
-  (req, res, next) => {
-    req.time = new Date().toString();
-    next();
-  },
-  (req, res) => {
-    res.send({time: req.time});
+app.get(
+  '/:word/echo', (req, res) => {
+    res.send({echo: req.params.word})
   }
-);
+)
 
 
 
