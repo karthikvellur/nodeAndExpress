f5930c92eeba7ca2bd51dc49719f2521469dca8a
Karthik Vellur <karthikvn23@gmail.com>
2020-12-06 08:50:20 +0000

    Middleware at Root Level

--- myApp.js
+++ myApp.js
@@ -3,21 +3,29 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/views/index.html');
-// })
+app.use(
+  (req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+  }
+);
 
-app.get('/json', function(req, res){
-  res.json({message: "Hello json"});
-});
-
-process.env.MESSAGE_STYLE="uppercase"
-app.get("/json", (req, res) => {
-  if(process.env.MESSAGE_STYLE === 'uppercase')
-    res.json({"message": "HELLO JSON"})
-  else
-    res.json({"message": "Hello json"})
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/views/index.html');
 })
+
+// app.get("/json", (req, res) => {
+//   res.json({"message": "Hello json"});
+//   // console.log({message: "Hello json"});
+// });
+
+// process.env.MESSAGE_STYLE="uppercase"
+// app.get("/json", (req, res) => {
+//   if(process.env.MESSAGE_STYLE === 'uppercase')
+//     res.json({"message": "HELLO JSON"})
+//   else
+//     res.json({"message": "Hello json"})
+// })
 
 
 
