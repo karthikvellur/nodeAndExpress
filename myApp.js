3b336a3d99f3873b90c832b28305d68d7c6ea828
Glitch (beneficial-steadfast-comfort) <none>
2020-12-06 13:13:50 +0000

    middleware chaining

--- myApp.js
+++ myApp.js
@@ -1,31 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
-
-app.use(
+app.get("/now",
   (req, res, next) => {
-    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    req.time = new Date().toString();
     next();
+  },
+  (req, res) => {
+    res.send({time: req.time});
   }
 );
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/views/index.html');
-})
-
-// app.get("/json", (req, res) => {
-//   res.json({"message": "Hello json"});
-//   // console.log({message: "Hello json"});
-// });
-
-// process.env.MESSAGE_STYLE="uppercase"
-// app.get("/json", (req, res) => {
-//   if(process.env.MESSAGE_STYLE === 'uppercase')
-//     res.json({"message": "HELLO JSON"})
-//   else
-//     res.json({"message": "Hello json"})
-// })
 
 
 
