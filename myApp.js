0047cddbb317713a055c094792c3f4cd1c26eb2c
Glitch (beneficial-steadfast-comfort) <none>
2020-12-07 04:23:54 +0000

    code snippets for all challenges

--- myApp.js
+++ myApp.js
@@ -13,30 +13,35 @@ app.get('/html', (req, res) => {
 // Serve JSON for specific route -- https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/serve-json-on-a-specific-route
 // Use .env file -- https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/use-the--env-file
 
-// Root Level logger: https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/implement-a-root-level-request-logger-middleware
+// Root Level logger -- https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/implement-a-root-level-request-logger-middleware
 app.use( (req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
-// Chain Middleware to create timer: https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/chain-middleware-to-create-a-time-server
+// Chain Middleware to create timer -- https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/chain-middleware-to-create-a-time-server
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.send({time: req.time});
+});
 
-
-// Path Params : 
+// Path Params -- https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/get-route-parameter-input-from-the-client
 app.get(
   '/:word/echo', (req, res) => {
     res.send({echo: req.params.word});
   }
 );
 
-// Query Params
+// Query Params -- https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/get-query-parameter-input-from-the-client
 app.get(
   '/name', (req, res) => {
     res.send({name: `${req.query.first} ${req.query.last}`})
   }
 )
 
-// Mounting bodyParser for 
+// Mounting bodyParser for parsing POST requests -- https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/use-body-parser-to-parse-post-requests
 app.use(bodyParser.urlencoded({extended: false}));
 
 
